a7d23aa7a3a33506ff7f7aca79c689650980d288
Tim Caswell <tim@creationix.com>
2009-10-01 13:00:35 -0500

    Add login page and make haml compile in a specified scope

--- dinmaker.js
+++ dinmaker.js
@@ -1,35 +1,47 @@
 include('/utils.js');
 var server = require("http_server.js");
 var haml = require("haml.js");
-
-// haml.render({}, 'interface.haml', function (html) {
-//   puts(html);
-// });
-
+var listeners = {};
 
 // This is the handler for long-poll requests.  We don't want to respond to these right away, but wait till
 // there is something to report.  We will set up an event listener that closes the connection upon an event.
 function long_poll(req, res, username) {
- 
+ listeners[username] = function (message) {
+   puts(JSON.stringify({username: username, message: message}));
+   res.finish();
+ }
 }
 
 function send_message(req, res, username, message) {
+  for (name in listeners) {
+    if (listeners.hasOwnProperty(name)) {
+      (listeners[name])(message);
+    }
+  }
  puts("Message" + message);
- res.close();
+ res.finish();
 }
 
-
-// Render the interface.haml file for requests to the base path
-server.get(/^\/$/, function (req, res) {
- haml.render({}, 'interface.haml', function (html) {
-   res.simpleHtml(200, html);
- });
-});
 
 // Serve js, css, and png files as static resources
 server.get(/^(\/.+\.(?:js|css|png|ico|tci))$/, function (req, res, path) {
  server.staticHandler(req, res, "public" + path);
 });
+
+// Render the login window
+server.get(/^\/$/, function (req, res) {
+ haml.render({}, 'login.haml', function (html) {
+   res.simpleHtml(200, html);
+ });
+});
+
+// Render the chat interface
+server.get(/^\/([^\/]*)$/, function (req, res, username) {
+ haml.render({username: username}, 'interface.haml', function (html) {
+   res.simpleHtml(200, html);
+ });
+});
+
 
 // Handle long_poll requests
 server.get(/^\/listen\/(.*)$/, long_poll);
--- public/Demo.js
+++ public/Demo.js
@@ -1,19 +1,52 @@
 /*jslint white: true, onevar: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, newcap: true, immed: true */
 /*globals TC, jQuery*/
 
-TC.defineController("Demo", function (params) {
+TC.defineController("Demo", function (username) {
+  
+  var $ = jQuery, self = this, messages = [], users = {};
 	
 	this.bindData("selected_users", {value: {}});
+	this.bindData("new_message", {value: ""});
+	
+	this.send_message = function () {
+	  var message = self.new_message.get();
+	  if (message === '') {
+	    return;
+	  }
+	  $.ajax({
+      type: "POST",
+      url: "/message/" + username,
+      processData: false,
+      data: message
+    });
+    self.new_message.set("");
+	}
+	
 	this.table_model = {
 	  get_num_rows: function () {
-	    return 1;
+	    return 2;
 	  },
 	  get_value_at: function (row_index, column) {
-	    return "Tim";
+	    return ["Tim", "Bob"][row_index];
 	  }
-	  
-	  
-	  
 	};
+	
+	function long_poll() {
+	  $.ajax({
+      type: "POST",
+      url: "/listen/" + username,
+      success: function (data) {
+        console.log(data);
+        long_poll();
+      },
+      error: function (err) {
+        console.error("Long-Poll Error, retrying in 10 seconds...");
+        setTimeout(long_poll, 10000);
+      }
+    });
+	}
+	
+	// Kick off the long poll process on load
+	$(long_poll);
 
 });
